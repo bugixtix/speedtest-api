f9a3391dc93b4cdbe39ad2965558fd62d855abc8
Ixtix <ixtix_bug@hotmail.com>
2024-12-11 00:28:40 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+import speedtest
+
+app = Flask(__name__)
+
+@app.rout('/speedtest', method=['GET'])
+def speedtest_handler():
+    st = speedtest.Speedtest()
+    st.get_best_server()
+    ping = st.results.ping
+    download = st.download() / 1_000_000
+    upload = st.upload() / 1_000_000
+    return jsonify({'ping':ping, 'download':download, 'upload':upload})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
