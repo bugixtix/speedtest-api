7a8b977a43c769b14fe41505ff0b3d169b3b6d24
Housam_ <ixtix_bug@hotmail.com>
2024-12-18 19:58:30 +0100

    Add: Home route

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import speedtest
+import time
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/speedtest', methods=['GET'])
 def speedtest_handler():
     st = speedtest.Speedtest()
     st.get_best_server()
     ping = st.results.ping
-    download = st.download() / 1_000_000
-    upload = st.upload() / 1_000_000
-    return jsonify({'ping':ping, 'download':download, 'upload':upload})
+    download = st.download()
+    upload = st.upload()
+    #Convert and return the values in Mbps
+    return jsonify({'ping':ping, 'download':round(download/1e6, 2), 'upload':round(upload/1e6,2)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
